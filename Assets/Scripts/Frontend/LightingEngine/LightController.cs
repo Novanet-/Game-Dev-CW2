17c0eff32560b1483cba690fa376f8f4f7ebcba5
Novanet <gamemaster8888@gmail.com>
2017-04-19 21:48:44 +0100

    Added intensity strength multiplier fields

--- Assets/Scripts/Frontend/LightingEngine/LightController.cs
+++ Assets/Scripts/Frontend/LightingEngine/LightController.cs
@@ -3,33 +3,35 @@ using UnityEngine;
 
 namespace Frontend
 {
-    public class LightController : MonoBehaviour
-    {
-        #region Private Fields
+	public class LightController : MonoBehaviour
+	{
+		#region Private Fields
 
 		[SerializeField] private float _daylength;
 		private Light[] _lights;
 		private float _timeOfDay;
+		[SerializeField] private float _daylightStrengthMult = 2.0f;
+		[SerializeField] private float _nightlightStrengthMult = 2.0f;
 
-        #endregion Private Fields
+		#endregion Private Fields
 
-        #region Private Methods
+		#region Private Methods
 
-        // Use this for initialization
-        private void Start()
-        {
+		// Use this for initialization
+		private void Start()
+		{
 			_lights = GetComponentsInChildren<Light>();
-        }
+		}
 
-        // Update is called once per frame
-        private void Update()
-        {
+		// Update is called once per frame
+		private void Update()
+		{
 			_timeOfDay = Time.time / _daylength;
 			transform.rotation = Quaternion.Euler(new Vector3(0, 0, _timeOfDay * 360));
-			_lights[0].intensity = Mathf.Sin(_timeOfDay * Mathf.PI);
-			_lights[1].intensity = 1 - Mathf.Sin(_timeOfDay * Mathf.PI);
-        }
+			_lights[0].intensity = Mathf.Sin(_timeOfDay * Mathf.PI) * _daylightStrengthMult;
+			_lights[1].intensity = 1 - Mathf.Sin(_timeOfDay * Mathf.PI) * _nightlightStrengthMult;
+		}
 
-        #endregion Private Methods
-    }
+		#endregion Private Methods
+	}
 }
